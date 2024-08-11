645854bd6d838eaa339fc8bb36d5b01625f288a9
musharaf <107387041+mushraf6461@users.noreply.github.com>
2024-08-11 22:15:36 +0530

    Update contact7_download_06.php

--- plugin-customization/contact7_download_06.php
+++ plugin-customization/contact7_download_06.php
@@ -1,7 +1,7 @@
 <script>
 document.addEventListener( 'wpcf7mailsent', function( event ) {
-  if ( '5458' == event.detail.contactFormId ) {
-    location = 'https://www.ecloudcontrol.com/wp-content/uploads/2022/12/Signature-SRE-Dec2022-v8.pdf';
+  if ( '8315' == event.detail.contactFormId ) {
+    location = 'https://www.ecloudcontrol.com/wp-content/uploads/2023/12/White-Paper_-Migrating-Data-from-Sybase-to-Azure-SQL-Server-v7.pdf';
   }
 }, false );
 </script>
